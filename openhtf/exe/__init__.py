6e4875bcf26bba23b5d646cc80e1c18956dd6c4e
Madeline Chan <maddychan@maddychan-macbookair.roam.corp.google.com>
2016-07-01 09:40:22 -0700

    Edited examples folder

--- openhtf/exe/__init__.py
+++ openhtf/exe/__init__.py
@@ -58,7 +58,7 @@ class TestExecutor(threads.KillableThread):
     # when shutting down, such as in response to a SIGINT.
     self._teardown_function = (
         teardown_function and
-        openhtf.PhaseDescriptor.WrapOrCopy(
+        openhtf.PhaseDescriptor.wrap_or_copy(
             teardown_function, timeout_s=conf.teardown_timeout_s))
         
     self._test_descriptor = test_descriptor
